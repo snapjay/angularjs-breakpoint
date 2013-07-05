5f47f98bd070a1dbcc3b45db0ccf3f6248d80b82
= <dan@snapjay.com>
2013-07-05 11:55:08 -0400

    Serve JS local

--- breakpoint.0.1.js
+++ breakpoint.0.1.js
@@ -1,7 +1,59 @@
-/**
- * Created with IntelliJ IDEA.
- * User: danshreim
- * Date: 05/07/13
- * Time: 11:52 AM
- * To change this template use File | Settings | File Templates.
+var breakpointApp = angular.module('breakpointApp',[]);
+
+/***
+ *
+ * BREAKPOINT DIRECTIVE FOR RESPONSIVE WEBSITES
+ *
+ *  http://snapjay.github.com/angularjs-breakpoint/
+ *  Apply as a attribute of the body tag.  Set
+ *  breakpoint="{1250:'break1250', 1000:'break1000',1120:'break1120'}
+ *
+ *  Values are available on scope as
+ *  {{breakpoint.class}} = current set class
+ *  {{breakpoint.windowSize}} = current width of window
+ *
  */
+
+breakpointApp.directive('breakpoint', ['$window', '$rootScope', function($window, $rootScope){
+    return {
+        restrict:"A",
+        link:function(scope, element, attr){
+            scope.breakpoint = {class:'', windowSize:$window.innerWidth } // Initialise Values;
+
+            var breakpoints = (scope.$eval(attr.breakpoint));
+
+            angular.element($window).bind('resize', setWindowSize);
+
+            scope.$watch('breakpoint.windowSize', function(windowWidth, oldValue){
+                setClass(windowWidth);
+            });
+
+            scope.$watch('breakpoint.class', function(newClass, oldClass) {
+                if (newClass != oldClass) broadcastEvent();
+            });
+
+            function broadcastEvent (oldClass) {
+                $rootScope.$broadcast('breakpointChange', scope.breakpoint, oldClass);
+            }
+
+            function setWindowSize (){
+                scope.breakpoint.windowSize = $window.innerWidth;
+                scope.$apply();
+            }
+
+            function setClass(windowWidth){
+                var setClass = breakpoints[Object.keys(breakpoints)[0]];
+                for (var breakpoint in breakpoints){
+                    if (breakpoint < windowWidth) setClass = breakpoints[breakpoint];
+                    element.removeClass(breakpoints[breakpoint]);
+                }
+                element.addClass(setClass);
+                scope.breakpoint.class  = setClass;
+                scope.$apply();
+            }
+        }
+    }
+}]);
+
+
+
